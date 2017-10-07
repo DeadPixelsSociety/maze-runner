05817cf4033c533c077df8dfeec8ea577cc808ba
BenGoodwin25 <goodb25@gmail.com>
2017-10-07 15:59:09 +0200

    Added Licence to Network.h

--- src/local/Network.h
+++ src/local/Network.h
@@ -1,3 +1,21 @@
+/*
+ * Run and Crawl - A student indie game
+ * Copyright (C) 2017-2018 to authors see AUTHORS file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _NETWORK_LOCAL_H
 #define _NETWORK_LOCAL_H
 
